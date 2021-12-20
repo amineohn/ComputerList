99cea6c6a83ce9aa873bc249599e4fab904708fe
amineohn <amineprojet7@gmail.com>
2021-12-20 11:03:20 +0100

    fix eslint

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -64,9 +64,9 @@ const Hello = () => {
                       stroke="currentColor"
                     >
                       <path
-                        stroke-linecap="round"
+                        strokeLinecap="round"
                         stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeWidth="2"
                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                       ></path>
                     </svg>
@@ -109,9 +109,9 @@ const Hello = () => {
                         stroke="currentColor"
                       >
                         <path
-                          stroke-linecap="round"
+                          strokeLinecap="round"
                           stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeWidth="2"
                           d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                         ></path>
                       </svg>
--- pages/index.tsx
+++ pages/index.tsx
@@ -113,9 +113,9 @@ const Login: NextPage = () => {
                     stroke="currentColor"
                   >
                     <path
-                      stroke-linecap="round"
+                      strokeLinecap="round"
                       stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeWidth="2"
                       d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                     ></path>
                   </svg>
@@ -158,9 +158,9 @@ const Login: NextPage = () => {
                       stroke="currentColor"
                     >
                       <path
-                        stroke-linecap="round"
+                        strokeLinecap="round"
                         stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeWidth="2"
                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                       ></path>
                     </svg>
