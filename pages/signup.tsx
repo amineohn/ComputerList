6862a0f9f1647499003d265625202484625cfaf4
amineohn <amineprojet7@gmail.com>
2021-12-27 10:27:59 +0100

    add toast

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -241,7 +241,7 @@ const SignUp = () => {
                 onChange={(e) => onChange(e)}
               />
             </div>
-            <div className="mb-6">
+            <div className="mb-6 space-y-2">
               <label
                 className="block text-neutral-50 text-sm font-bold mb-2"
                 htmlFor="password"
@@ -258,9 +258,46 @@ const SignUp = () => {
                 onChange={(e) => onChange(e)}
               />
               {password.length < 6 && (
-                <p className="text-neutral-50 text-xs italic">
-                  Password must contain at least 6 characters
-                </p>
+                <div
+                  className="bg-pink-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block pt-2"
+                  id="static-example"
+                  role="alert"
+                  aria-live="assertive"
+                  aria-atomic="true"
+                  data-mdb-autohide="false"
+                >
+                  <div className="bg-pink-500 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-pink-500 rounded-t-lg">
+                    <p className="font-bold text-white flex items-center">
+                      <svg
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        data-icon="times-circle"
+                        className="w-4 h-4 mr-2 fill-current text-neutral-50"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                      >
+                        <path
+                          className="text-neutral-50"
+                          d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"
+                        ></path>
+                      </svg>
+                      Requirements
+                    </p>
+                    <div className="flex items-center">
+                      <button
+                        type="button"
+                        className="btn-close btn-close-white box-content w-4 h-4 ml-2 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline"
+                        data-mdb-dismiss="toast"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                  </div>
+                  <div className="p-3 bg-pink-600 rounded-b-lg break-words text-white">
+                    Password must be at least 6 characters long
+                  </div>
+                </div>
               )}
             </div>
 
